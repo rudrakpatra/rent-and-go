21dac8235b302409303396f44c7a3b79b3bb9fd4
rudrakpatra <rudrakpatracodes@gmail.com>
2022-03-27 18:08:49 +0530

    frontend 03

--- src/lib/pages/Orders.svelte
+++ src/lib/pages/Orders.svelte
@@ -1,5 +1,23 @@
-<h1>orders page</h1>
-<div>
-    <h2>What is Lorem Ipsum?</h2>
-    <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<div class="mdc-typography--headline1">
+    Headline 1
 </div>
+ 
+<div class="mdc-typography--headline2">
+    Headline 2
+</div>
+ 
+<div class="mdc-typography--headline3">
+    Headline 3
+</div>
+<div class="mdc-typography--headline3">
+    <div class="mdc-typography--headline3">
+        What is Lorem Ipsum?
+    </div>
+    <p>
+        <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+</div>
+
+<style lang="scss">
+    @use "@material/typography/mdc-typography";
+</style>
